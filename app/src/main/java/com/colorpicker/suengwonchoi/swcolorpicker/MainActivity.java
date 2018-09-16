75f0cbef929c77d9167191b951fb2348e58792ab
Sueng-Won <yuventius125@icloud.com>
2018-09-16 17:22:42 +0900

    git 상태확인

--- app/src/main/java/com/colorpicker/suengwonchoi/swcolorpicker/MainActivity.java
+++ app/src/main/java/com/colorpicker/suengwonchoi/swcolorpicker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.colorpicker.suengwonchoi.swcolorpicker;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+//    public void onButtonClicked(View v){
+//        Toast.makeText(getApplicationContext(), "버튼누름",Toast.LENGTH_LONG).show();
+//    }
+    public void gotoNaver(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void callToMe(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222"));
+        startActivity(intent);
     }
 }
